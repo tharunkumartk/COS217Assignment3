82e519fdf00421ac8e5fc4e243772b18b45096c0
TharunTK <tharun.tiruppali@gmail.com>
2022-10-27 01:14:14 -0400

    upd

--- symtable.h
+++ symtable.h
@@ -14,15 +14,15 @@ int SymTable_put(SymTable_T oSymTable,
    const char *pcKey, const void *pvValue);
 
 void *SymTable_replace(SymTable_T oSymTable,
-     const char *pcKey, const void *pvValue);
+   const char *pcKey, const void *pvValue);
 
-  int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
+int SymTable_contains(SymTable_T oSymTable, const char *pcKey);
 
-  void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
+void *SymTable_get(SymTable_T oSymTable, const char *pcKey);
 
-  void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
+void *SymTable_remove(SymTable_T oSymTable, const char *pcKey);
 
-  void SymTable_map(SymTable_T oSymTable,
-     void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
-     const void *pvExtra);
+void SymTable_map(SymTable_T oSymTable,
+   void (*pfApply)(const char *pcKey, void *pvValue, void *pvExtra),
+   const void *pvExtra);
 #endif
