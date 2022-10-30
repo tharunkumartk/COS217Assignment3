0dff0514ebe32d9338f337e54681b9c38028368d
TharunTK <tharun.tiruppali@gmail.com>
2022-10-30 15:24:13 -0400

    asdasd

--- symtablehash.c
+++ symtablehash.c
@@ -139,7 +139,7 @@ int SymTable_put(SymTable_T oSymTable,
     assert(pcKey != NULL);
     if(oSymTable->size == *(oSymTable->uBucketCount)) {
         if(SymTable_expand(oSymTable)==0)
-            return 0
+            return 0;
     }
     index = SymTable_hash(pcKey, *(oSymTable->uBucketCount));
     for (pCurrentBinding = oSymTable->head[index];
